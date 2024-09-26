2f6470132dde7dd83a8dfad3af5692295bed9e13
Shubham Gandhi <sg9769356@gmail.com>
2024-09-26 19:16:38 +0530

    add some new simple cases

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,101 +1,102 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function main() {
   const india = await prisma.team.create({
     data: {
-      name: "India",
+      name: 'India',
       players: {
         create: [
-          { name: "Rohit Sharma" },
-          { name: "Virat Kohli" },
-          { name: "Shubman Gill" },
-          { name: "KL Rahul" },
-          { name: "Hardik Pandya" },
-          { name: "Ravindra Jadeja" },
-          { name: "Jasprit Bumrah" },
-          { name: "Mohammed Shami" },
-          { name: "Kuldeep Yadav" },
-          { name: "Shardul Thakur" },
-          { name: "Ishan Kishan" },
+          { name: 'Rohit Sharma' },
+          { name: 'Virat Kohli' },
+          { name: 'Shubman Gill' },
+          { name: 'KL Rahul' },
+          { name: 'Hardik Pandya' },
+          { name: 'Ravindra Jadeja' },
+          { name: 'Jasprit Bumrah' },
+          { name: 'Mohammed Shami' },
+          { name: 'Kuldeep Yadav' },
+          { name: 'Shardul Thakur' },
+          { name: 'Ishan Kishan' },
         ],
       },
+      isBatting: true,
     },
-  });
+  })
 
   const australia = await prisma.team.create({
     data: {
-      name: "Australia",
+      name: 'Australia',
       players: {
         create: [
-          { name: "David Warner" },
-          { name: "Steve Smith" },
-          { name: "Marnus Labuschagne" },
-          { name: "Travis Head" },
-          { name: "Glenn Maxwell" },
-          { name: "Pat Cummins" },
-          { name: "Mitchell Starc" },
-          { name: "Josh Hazlewood" },
-          { name: "Alex Carey" },
-          { name: "Marcus Stoinis" },
-          { name: "Cameron Green" },
+          { name: 'David Warner' },
+          { name: 'Steve Smith' },
+          { name: 'Marnus Labuschagne' },
+          { name: 'Travis Head' },
+          { name: 'Glenn Maxwell' },
+          { name: 'Pat Cummins' },
+          { name: 'Mitchell Starc' },
+          { name: 'Josh Hazlewood' },
+          { name: 'Alex Carey' },
+          { name: 'Marcus Stoinis' },
+          { name: 'Cameron Green' },
         ],
       },
     },
-  });
+  })
   const match = await prisma.matchStats.create({
     data: {},
-  });
+  })
 
-  const players = await prisma.player.findMany();
+  const players = await prisma.player.findMany()
   for (const player of players) {
-    if (player.name === "Josh Hazlewood") {
+    if (player.name === 'Josh Hazlewood') {
       await prisma.playerStats.create({
         data: {
           playerId: player.id,
           currentlyBowling: true,
         },
-      });
+      })
 
-      continue;
+      continue
     }
 
-    if (player.name === "Rohit Sharma") {
+    if (player.name === 'Rohit Sharma') {
       await prisma.playerStats.create({
         data: {
           playerId: player.id,
           currentlyOnStrike: true,
         },
-      });
+      })
 
-      continue;
+      continue
     }
 
-    if (player.name === "Virat Kohli") {
+    if (player.name === 'Virat Kohli') {
       await prisma.playerStats.create({
         data: {
           playerId: player.id,
           currentlyNonStriker: true,
         },
-      });
+      })
 
-      continue;
+      continue
     }
     const playerStats = await prisma.playerStats.create({
       data: {
         playerId: player.id,
       },
-    });
+    })
   }
 
-  console.log("Seeding completed.");
+  console.log('Seeding completed.')
 }
 main()
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
